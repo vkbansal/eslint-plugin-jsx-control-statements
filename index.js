f2b2bc2417d69f137c8cc2557b162a64f926a86b
Vivek Kumar Bansal <vkb0310@gmail.com>
2016-01-07 11:22:14 +0530

    include new rules

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 module.exports = {
     rules: {
         "jsx-if-condition": require("./lib/rules/jsx-if-condition"),
-        "jsx-if-single-child": require("./lib/rules/jsx-if-single-child")
+        "jsx-if-single-child": require("./lib/rules/jsx-if-single-child"),
+        "jsx-use-if-tag": require("./lib/rules/jsx-use-if-tag"),
+        "jsx-for-single-child": require("./lib/rules/jsx-for-single-child")
     }
 };
